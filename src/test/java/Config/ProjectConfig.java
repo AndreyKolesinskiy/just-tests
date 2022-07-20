257065ccea3b543d15b65e2d38067d32b5088507
Andrei Kaliasinski <andykolesinski@gmail.com>
2022-07-20 22:48:13 +0300

    Added infrastructure

---
+++ src/test/java/Config/ProjectConfig.java
@@ -0,0 +1,36 @@
+package Config;
+
+import org.aeonbits.owner.Config;
+
+@Config.LoadPolicy(Config.LoadType.MERGE)
+@Config.Sources({
+        "system:properties",
+        "classpath:config/local.properties",
+        "classpath:config/remote.properties"
+})
+
+public interface ProjectConfig extends Config {
+    @DefaultValue("chrome")
+    @Key("browser")
+    String browser();
+
+    @DefaultValue("91.0")
+    @Key("browserVersion")
+    String browserVersion();
+
+    @DefaultValue("1920x1080")
+    @Key("browserSize")
+    String browserSize();
+
+    @DefaultValue("")
+    @Key("browserMobileView")
+    String browserMobileView();
+
+    @DefaultValue("")
+    @Key("remoteDriverUrl")
+    String remoteDriverUrl();
+
+    @DefaultValue("")
+    @Key("videoStorage")
+    String videoStorage();
+}
---
+++ src/test/java/tests/BaseTest.java
@@ -0,0 +1,35 @@
+package tests;
+
+import Config.Project;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.AllureAttachments;
+import helpers.DriverSettings;
+import helpers.DriverUtils;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+
+public class BaseTest {
+
+    @BeforeAll
+    static void setUp() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+        DriverSettings.configure();
+    }
+
+    @AfterEach
+    public void addAttachments(){
+        String sessionId = DriverUtils.getSessionId();
+
+        AllureAttachments.addScreenshotAs("Last screenshot");
+        AllureAttachments.addPageSource();
+        AllureAttachments.addBrowserConsoleLogs();
+
+        Selenide.closeWebDriver();
+
+        if (Project.isVideoOn()) {
+            AllureAttachments.addVideo(sessionId);
+        }
+    }
+}
